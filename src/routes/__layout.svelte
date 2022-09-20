e12dae57908d5fd592c81d6934228cdbc37dea08
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-09-20 14:36:11 +0100

    conditional cdn load

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,11 +2,24 @@
   import "../app.css";
   import ONSHeader from "$lib/layout/ONSHeader.svelte";
   import Title from "$lib/layout/Title.svelte";
+  import { page } from '$app/stores';
+
+
+  let path = $page.url.pathname;
+
+  console.error('£££££££££££££££"',path)
+
+
 </script>
 
+
+
 <svelte:head>
+  {#if path === '/build'}
   <script src="https://cdn.jsdelivr.net/npm/danfojs@1.1.1/lib/bundle.js"></script>
+  {:else if path === '/draw'}
   <script src="https://cdn.jsdelivr.net/npm/flatbush"></script>
+  {/if}
 
   <title>Build a custom area profile</title>
 </svelte:head>
