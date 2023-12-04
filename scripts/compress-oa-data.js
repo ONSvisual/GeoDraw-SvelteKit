d8246da85217cc75bd937a0ed213136f84285060
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2023-12-04 12:40:56 +0000

    change to lsoa metadata

--- scripts/compress-oa-data.js
+++ scripts/compress-oa-data.js
@@ -2,24 +2,40 @@ import fs from "fs";
 import { csvParse, autoType } from "d3-dsv";
 import { compressData } from "compress-csv-to-json";
 
-const input = "./raw_data/oa21-data.csv";
-const output = "./static/data/oa21-data.json";
+const parse = (path) => csvParse(
+    fs.readFileSync(path, {encoding: 'utf8', flag: 'r'}).replace(/\uFEFF/, ''),
+    autoType
+);
+const input = (yr) => `./raw_data/lsoa${yr}-data.csv`;
+const output = (yr) => `./static/data/lsoa${yr}-data.json`;
 
-const raw = fs.readFileSync(input, {encoding: 'utf8', flag: 'r'});
-const data = csvParse(raw.replace(/\uFEFF/, ''), autoType);
+const data11 = parse(input("11"));
+const data21 = parse(input("21"));
 
-const columnTypes = [
-    {key: 'oa21cd', colType: 'string'},
-    {key: 'lsoa21cd', colType: 'interned_string'},
-    {key: 'msoa21cd', colType: 'interned_string'},
-    {key: 'ltla21cd', colType: 'interned_string'},
-    {key: 'rgn21cd', colType: 'interned_string'},
-    {key: 'lng', colType: 10_000},
-    {key: 'lat', colType: 10_000},
-    {key: 'population', colType: 1},
-];
+const compressed11 = compressData(
+    data11,
+    [
+        {key: 'lsoa11cd', colType: 'interned_string'},
+        {key: 'lsoa21cd', colType: 'interned_string'},
+        {key: 'msoa11cd', colType: 'interned_string'},
+        {key: 'ltla11cd', colType: 'interned_string'},
+        {key: 'rgn11cd', colType: 'interned_string'},
+    ]
+);
+const compressed21 = compressData(
+    data21,
+    [
+        {key: 'lsoa21cd', colType: 'string'},
+        {key: 'msoa21cd', colType: 'interned_string'},
+        {key: 'ltla21cd', colType: 'interned_string'},
+        {key: 'rgn21cd', colType: 'interned_string'},
+        {key: 'lng', colType: 10_000},
+        {key: 'lat', colType: 10_000},
+        {key: 'population', colType: 1},
+    ]
+);
 
-const compressed = compressData(data, columnTypes);
-
-fs.writeFileSync(output, JSON.stringify(compressed));
-console.log(`Wrote ${output}`);
+fs.writeFileSync(output("11"), JSON.stringify(compressed11));
+console.log(`Wrote ${output("11")}`);
+fs.writeFileSync(output("21"), JSON.stringify(compressed21));
+console.log(`Wrote ${output("21")}`);
